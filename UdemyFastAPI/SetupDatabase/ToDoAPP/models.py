f831d5f73c99ae0fac74d395459b7d0bfb6b2ef5
RasulAbduvaitov <96064378+Rasulid@users.noreply.github.com>
2023-01-04 19:49:44 +0500

    over data base

--- UdemyFastAPI/SetupDatabase/ToDoAPP/models.py
+++ UdemyFastAPI/SetupDatabase/ToDoAPP/models.py
@@ -1,5 +1,7 @@
 from sqlalchemy import Boolean, Column, Integer, String
 from DataBase import Base
+from pydantic import BaseModel, Field
+from typing import Optional
 
 
 class Todos(Base):
@@ -12,11 +14,17 @@ class Todos(Base):
         complete = Bool , default = False
 
     """
-    __tablename__ = "todos"  #определяем имя таблицы
+    __tablename__ = "todos"  # определяем имя таблицы
 
-    id = Column(Integer, primary_key=True , index=True)
+    id = Column(Integer, primary_key=True, index=True)
     title = Column(String)
     description = Column(String)
     priority = Column(Integer)
     complete = Column(Boolean, default=False)
 
+
+class ToDo(BaseModel):
+    title: str
+    description: Optional[str]
+    priority: int = Field(gt=0, lt=6, description='The priority must be between 1-5 ')
+    complete: bool
