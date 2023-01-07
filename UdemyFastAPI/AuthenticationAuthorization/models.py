c468e9c15e8d5374c4cc9e1e20afa6e616bf52c7
RasulAbduvaitov <96064378+Rasulid@users.noreply.github.com>
2023-01-07 14:56:24 +0500

    Error code

---
+++ UdemyFastAPI/AuthenticationAuthorization/models.py
@@ -0,0 +1,38 @@
+from pydantic import BaseModel, Field
+from typing import Optional
+from sqlalchemy import Boolean, Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+from DataBase import Base
+
+
+class Users(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True, index=True)
+    username = Column(String, unique=True, index=True)
+    first_name = Column(String)
+    last_name = Column(String)
+    hashed_password = Column(String)
+    is_active = Column(Boolean, default=True)
+
+    todos = relationship("Todos", back_populates="owner")
+
+
+class Todos(Base):
+    __tablename__ = "todos"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String)
+    description = Column(String)
+    priority = Column(Integer)
+    complete = Column(Boolean, default=False)
+    owner_id = Column(Integer, ForeignKey("users.id"))
+
+    owner = relationship("Users", back_populates="todos")
+
+class ToDo(BaseModel):
+    title: str
+    description: Optional[str]
+    priority: int = Field(gt=0, lt=6, description='The priority must be between 1-5 ')
+    complete: bool
